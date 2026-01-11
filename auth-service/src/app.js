90c35ffd0247f80d0398504503e448e43aeb263c
Amélie <amelie.beaufils95@gmail.com>
2026-01-11 20:52:40 +0100

    feat: add reputation in auth-service

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/authRoute.js";
 import adminRoutes from "./routes/adminRoute.js";
+import internalRoutes from "./routes/reputationRoute.js";
 // Pour le token JWT
 import dotenv from "dotenv";
 dotenv.config();
@@ -10,5 +11,6 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/reputation", internalRoutes);
 
 export default app;
--- mongo-service/routes/LoreRoutes.js
+++ mongo-service/routes/LoreRoutes.js
@@ -1,17 +1,21 @@
-import express from 'express';
-import { authenticate } from '../middlewares/auth.js';
-import * as getCtrl from '../controllers/getController.js';
-import * as postCtrl from '../controllers/postController.js';
+import express from "express";
+import { authenticate } from "../middlewares/auth.js";
+import * as getCtrl from "../controllers/getController.js";
+import * as postCtrl from "../controllers/postController.js";
 
 const router = express.Router();
 
 // --- ROUTES GET ---
-router.get('/creatures', getCtrl.getAllCreatures);
-router.get('/testimonies', getCtrl.getAllTestimonies);
+router.get("/creatures", getCtrl.getAllCreatures);
+router.get("/testimonies", getCtrl.getAllTestimonies);
 
 // --- ROUTES POST ---
-router.post('/creatures', authenticate, postCtrl.createCreature);
-router.post('/testimonies', authenticate, postCtrl.createTestimony);
-router.post('/testimonies/:id/validate', authenticate, postCtrl.validateTestimony);
+router.post("/creatures", authenticate, postCtrl.createCreature);
+router.post("/testimonies", authenticate, postCtrl.createTestimony);
+router.post(
+  "/testimonies/:id/validate",
+  authenticate,
+  postCtrl.validateTestimony
+);
 
 export default router;
