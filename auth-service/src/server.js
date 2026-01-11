663a3dc8f2d461c002b68953dab3edc01afc255b
gamatrick <val@laflyfamily.com>
2026-01-11 11:48:06 +0100

    add .env to 3306 route localhost

--- auth-service/src/server.js
+++ auth-service/src/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => {
   console.log(`API running on http://localhost:${PORT}`);
 });
